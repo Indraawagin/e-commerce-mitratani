b69d1f453e2838204457c1bdf1edfbf7295f3956
Indraawagin <Indraawagin@gmail.com>
2022-07-14 02:27:34 +0800

    fix bug

--- resources/views/pages/category.blade.php
+++ resources/views/pages/category.blade.php
@@ -1,95 +1,99 @@
 @extends('layouts.app')
 @section('title')
-Mitra Tani - Category
+    Mitra Tani - Category
 @endsection
 
 @section('content')
-@push('addon-style')
-<style>
-    .store-products .owner {
-        font-weight: normal;
-        font-size: 14px;
-        line-height: 21px;
-        color: #979797;
-        margin-bottom: 5px;
-    }
-</style>
-@endpush
-<div class="page-content page-home">
-    <!-- Component Categories -->
-    <section class="store-categories">
-        <div class="container">
-            <div class="row">
-                <div class="col-12" data-aos="fade-up">
-                    <h5>Semua Kategori</h5>
+    @push('addon-style')
+        <style>
+            .store-products .owner {
+                font-weight: normal;
+                font-size: 14px;
+                line-height: 21px;
+                color: #979797;
+                margin-bottom: 5px;
+            }
+        </style>
+    @endpush
+    <div class="page-content page-home">
+        <!-- Component Categories -->
+        <section class="store-categories">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12" data-aos="fade-up">
+                        <h5>Semua Kategori</h5>
+                    </div>
                 </div>
-            </div>
-            <div class="row">
-                @php
-                $incrementCategory = 0;
-                @endphp
-                @forelse ($categories as $category)
-                <div class="col-6 col-md-4 col-lg-2" data-aos="fade-up" data-aos-delay="{{ $incrementCategory+= 100 }}">
-                    <a href="{{ route('categories-details', $category->slug) }}" class="component-categories d-block">
-                        <div class="categories-image">
-                            <img src="{{ Storage::url($category->photo) }}" alt="Pestisida" class="w-100" />
+                <div class="row">
+                    @php
+                        $incrementCategory = 0;
+                    @endphp
+                    @forelse ($categories as $category)
+                        <div class="col-6 col-md-4 col-lg-2" data-aos="fade-up"
+                            data-aos-delay="{{ $incrementCategory += 100 }}">
+                            <a href="{{ route('categories-details', $category->slug) }}"
+                                class="component-categories d-block">
+                                <div class="categories-image">
+                                    <img src="{{ Storage::url($category->photo) }}" alt="Pestisida" class="w-100" />
+                                </div>
+                                <p class="categories-text">{{ $category->name }}</p>
+                            </a>
                         </div>
-                        <p class="categories-text">{{ $category->name }}</p>
-                    </a>
+                    @empty
+                        <div class="col-12 text-center py-5" data-aos="fade-up"
+                            data-aos-delay="{{ $incrementProduct += 100 }}">
+                            Tidak Ada Kategori
+                        </div>
+                    @endforelse
                 </div>
-                @empty
-                <div class="col-12 text-center py-5" data-aos="fade-up" data-aos-delay="{{ $incrementProduct+= 100 }}">
-                    Tidak Ada Kategori
-                </div>
-                @endforelse
             </div>
-        </div>
-    </section>
+        </section>
 
-    <!-- Component Product -->
-    <section class="store-products">
-        <div class="container">
-            <div class="row">
-                <div class="col-12" data-aos="fade-up">
-                    <h5>Semua Produk</h5>
+        <!-- Component Product -->
+        <section class="store-products">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12" data-aos="fade-up">
+                        <h5>Semua Produk</h5>
+                    </div>
                 </div>
-            </div>
-            <div class="row">
-                @php
-                $incrementProduct = 0;
-                @endphp
-                @forelse ($products as $product)
-                <div class="col-6 col-md-4 col-lg-3" data-aos="fade-up" data-aos-delay="{{ $incrementProduct+= 100 }}">
-                    <a href="{{ route('detail', $product->slug) }}" class="component-products d-block">
-                        <div class="products-thumbnail">
-                            <div class="products-image" style="
-                                        @if ($product->galleries->count())
-                                            background-image: url('{{ Storage::url($product->galleries->first()->photos) }}')
+                <div class="row">
+                    @php
+                        $incrementProduct = 0;
+                    @endphp
+                    @forelse ($products as $product)
+                        <div class="col-6 col-md-4 col-lg-3" data-aos="fade-up"
+                            data-aos-delay="{{ $incrementProduct += 100 }}">
+                            <a href="{{ route('detail', $product->slug) }}" class="component-products d-block">
+                                <div class="products-thumbnail">
+                                    <div class="products-image"
+                                        style="
+                                        @if ($product->galleries->count()) background-image: url('{{ Storage::url($product->galleries->first()->photos) }}')
                                         @else
-                                            background-color: #eee
-                                        @endif
+                                            background-color: #eee @endif
 
                                         ">
-                            </div>
+                                    </div>
+                                </div>
+                                <div class="products-text">{{ $product->name }}</div>
+                                <div class="owner">Stok: {{ $product->stock }}</div>
+                                <div class="products-price">@currency($product->price)</div>
+                            </a>
                         </div>
-                        <div class="products-text">{{ $product->name }}</div>
-                        <div class="owner">Stok: {{ $product->stock }}</div>
-                        <div class="products-price">@currency($product->price)</div>
-                    </a>
+                    @empty
+                        <div class="col-12 text-center py-5" data-aos="fade-up"
+                            data-aos-delay="{{ $incrementProduct += 100 }}">
+                            <p class="mb-2">Tidak Ada Produk</p>
+                            <a href="{{ route('categories') }}" class="btn btn-secondary">Tampilkan</a>
+                        </div>
+                    @endforelse
                 </div>
-                @empty
-                <div class="col-12 text-center py-5" data-aos="fade-up" data-aos-delay="{{ $incrementProduct+= 100 }}">
-                    <p class="mb-2">Tidak Ada Produk</p>
-                    <a href="{{ route('categories') }}" class="btn btn-secondary">Tampilkan</a>
-                </div>
-                @endforelse
-            </div>
-            <div class="row">
-                <div class="col-12 mt-4">
-                    {{ $products->links() }}
+                <div class="row">
+                    <div class="col-12 mt-4">
+                        {{ $products->links() }}
+                    </div>
                 </div>
             </div>
-        </div>
-    </section>
-</div>
+        </section>
+    </div>
 @endsection
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,120 +1,125 @@
 @extends('layouts.app')
 @section('title')
-Mitra Tani - Homepage
+    Mitra Tani - Homepage
 @endsection
 
 @section('content')
-@push('addon-style')
-<style>
-    .store-products .owner {
-        font-weight: normal;
-        font-size: 14px;
-        line-height: 21px;
-        color: #979797;
-        margin-bottom: 5px;
-    }
-</style>
-@endpush
-<div class="page-content page-home">
-    <!-- Component Carousel -->
-    <section class="store-carousel">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12" data-aos="zoom-in">
-                    <div id="storeCarousel" class="carousel slide" data-ride="carousel">
-                        <ol class="carousel-indicators">
-                            <li class="active" data-target="#storeCarousel" data-slide-to="0"></li>
-                            <li data-target="#storeCarousel" data-slide-to="1"></li>
-                            <li data-target="#storeCarousel" data-slide-to="2"></li>
-                        </ol>
-                        <div class="carousel-inner">
-                            <div class="carousel-item active">
-                                <img src="{{ asset('/images/banner-toko.jpg') }}" alt="Carousel Image"
-                                    class="d-block w-100" />
-                            </div>
-                            <div class="carousel-item">
-                                <img src="{{ asset('/images/banner-roundup.jpg') }}" alt="Carousel Image"
-                                    class="d-block w-100" />
-                            </div>
-                            <div class="carousel-item">
-                                <img src="{{ asset('/images/banner-dangke.jpg') }}" alt="Carousel Image"
-                                    class="d-block w-100" />
+    @push('addon-style')
+        <style>
+            .store-products .owner {
+                font-weight: normal;
+                font-size: 14px;
+                line-height: 21px;
+                color: #979797;
+                margin-bottom: 5px;
+            }
+        </style>
+    @endpush
+    <div class="page-content page-home">
+        <!-- Component Carousel -->
+        <section class="store-carousel">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12" data-aos="zoom-in">
+                        <div id="storeCarousel" class="carousel slide" data-ride="carousel">
+                            <ol class="carousel-indicators">
+                                <li class="active" data-target="#storeCarousel" data-slide-to="0"></li>
+                                <li data-target="#storeCarousel" data-slide-to="1"></li>
+                                <li data-target="#storeCarousel" data-slide-to="2"></li>
+                            </ol>
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img src="{{ asset('/images/banner-toko.jpg') }}" alt="Carousel Image"
+                                        class="d-block w-100" />
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="{{ asset('/images/banner-roundup.jpg') }}" alt="Carousel Image"
+                                        class="d-block w-100" />
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="{{ asset('/images/banner-dangke.jpg') }}" alt="Carousel Image"
+                                        class="d-block w-100" />
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
+        </section>
 
-    <!-- Component Categories -->
-    <section class="store-categories">
-        <div class="container">
-            <div class="row">
-                <div class="col-12" data-aos="fade-up">
-                    <h5>Kategori</h5>
+        <!-- Component Categories -->
+        <section class="store-categories">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12" data-aos="fade-up">
+                        <h5>Kategori</h5>
+                    </div>
                 </div>
-            </div>
-            <div class="row">
-                @php
-                $incrementCategory = 0;
-                @endphp
-                @forelse ($categories as $category)
-                <div class="col-6 col-md-4 col-lg-2" data-aos="fade-up" data-aos-delay="{{ $incrementCategory+= 100 }}">
-                    <a href="{{ route('categories-details', $category->slug) }}" class="component-categories d-block">
-                        <div class="categories-image">
-                            <img src="{{ Storage::url($category->photo) }}" alt="Pestisida" class="w-100" />
+                <div class="row">
+                    @php
+                        $incrementCategory = 0;
+                        $incrementCategory = 0;
+                    @endphp
+                    @forelse ($categories as $category)
+                        <div class="col-6 col-md-4 col-lg-2" data-aos="fade-up"
+                            data-aos-delay="{{ $incrementCategory += 100 }}">
+                            <a href="{{ route('categories-details', $category->slug) }}"
+                                class="component-categories d-block">
+                                <div class="categories-image">
+                                    <img src="{{ Storage::url($category->photo) }}" alt="Pestisida" class="w-100" />
+                                </div>
+                                <p class="categories-text">{{ $category->name }}</p>
+                            </a>
                         </div>
-                        <p class="categories-text">{{ $category->name }}</p>
-                    </a>
+                    @empty
+                        <div class="col-12 text-center py-5" data-aos="fade-up"
+                            data-aos-delay="{{ $incrementProduct += 100 }}">
+                            Tidak Ada Kategori
+                        </div>
+                    @endforelse
                 </div>
-                @empty
-                <div class="col-12 text-center py-5" data-aos="fade-up" data-aos-delay="{{ $incrementProduct+= 100 }}">
-                    Tidak Ada Kategori
-                </div>
-                @endforelse
             </div>
-        </div>
-    </section>
+        </section>
 
-    <!-- Component Product -->
-    <section class="store-products">
-        <div class="container">
-            <div class="row">
-                <div class="col-12" data-aos="fade-up">
-                    <h5>Produk Terbaru</h5>
+        <!-- Component Product -->
+        <section class="store-products">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12" data-aos="fade-up">
+                        <h5>Produk Terbaru</h5>
+                    </div>
                 </div>
-            </div>
-            <div class="row">
-                @php
-                $incrementProduct = 0;
-                @endphp
-                @forelse ($products as $product)
-                <div class="col-6 col-md-4 col-lg-3" data-aos="fade-up" data-aos-delay="{{ $incrementProduct+= 100 }}">
-                    <a href="{{ route('detail', $product->slug) }}" class="component-products d-block">
-                        <div class="products-thumbnail">
-                            <div class="products-image" style="
-                            @if ($product->galleries->count())
-                                background-image: url('{{ Storage::url($product->galleries->first()->photos) }}')
+                <div class="row">
+                    @php
+                        $incrementProduct = 0;
+                    @endphp
+                    @forelse ($products as $product)
+                        <div class="col-6 col-md-4 col-lg-3" data-aos="fade-up"
+                            data-aos-delay="{{ $incrementProduct += 100 }}">
+                            <a href="{{ route('detail', $product->slug) }}" class="component-products d-block">
+                                <div class="products-thumbnail">
+                                    <div class="products-image"
+                                        style="
+                            @if ($product->galleries->count()) background-image: url('{{ Storage::url($product->galleries->first()->photos) }}')
                             @else
-                                background-color: #eee
-                            @endif
+                                background-color: #eee @endif
 
                             ">
-                            </div>
+                                    </div>
+                                </div>
+                                <div class="products-text">{{ $product->name }}</div>
+                                <div class="owner">Stok {{ $product->stock }}</div>
+                                <div class="products-price">@currency($product->price)</div>
+                            </a>
                         </div>
-                        <div class="products-text">{{ $product->name }}</div>
-                        <div class="owner">Stok {{ $product->stock }}</div>
-                        <div class="products-price">@currency($product->price)</div>
-                    </a>
+                    @empty
+                        <div class="col-12 text-center py-5" data-aos="fade-up"
+                            data-aos-delay="{{ $incrementProduct += 100 }}">
+                            Tidak Ada Produk
+                        </div>
+                    @endforelse
                 </div>
-                @empty
-                <div class="col-12 text-center py-5" data-aos="fade-up" data-aos-delay="{{ $incrementProduct+= 100 }}">
-                    Tidak Ada Produk
-                </div>
-                @endforelse
             </div>
-        </div>
-    </section>
-</div>
+        </section>
+    </div>
 @endsection
